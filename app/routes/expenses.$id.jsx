c5e5d8a30a4fadcd54c2b1c7bf953c14155bb0d5
Mykhailo Ostapysko <mykhailo.ostapysko@globant.com>
2023-03-13 18:47:05 +0200

    adds some links

--- app/routes/expenses.$id.jsx
+++ app/routes/expenses.$id.jsx
@@ -1,9 +1,15 @@
-
+import { useNavigate } from '@remix-run/react';
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
 
 export default function ViewEditExpensePage() {
+  const navigate = useNavigate();
+
+  const closeModal = () => navigate(-1);
+
   return (
-    <Modal><ExpenseForm /></Modal>
+    <Modal onClose={closeModal}>
+      <ExpenseForm />
+    </Modal>
   );
 }

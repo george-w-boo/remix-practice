1db706ab0f73bb7f7a70d20b5d311cd013fff0b8
Mykhailo Ostapysko <mp4clips@gmail.com>
2023-03-15 23:57:50 +0200

    handles empty expenses list

--- app/routes/_app.expenses.jsx
+++ app/routes/_app.expenses.jsx
@@ -6,21 +6,28 @@ import { getExpenses } from "../data/expenses.server";
 
 export default function ExpensesLayout() {
   const expenses = useLoaderData();
+
+  const hasExpenses = expenses && expenses.length > 0;
+
   return (
       <>
         <Outlet />
-        <section id="expenses-actions">
-          <Link to="add">
-            <FaPlus />
-            <span>Add Expense</span>
-          </Link>
-          <a href="/expenses/raw">
-            <FaDownload />
-            <span>Load Raw Data</span>
-          </a>
-        </section>
         <main>
-          <ExpensesList expenses={expenses} />
+          <section id="expenses-actions">
+            <Link to="add">
+              <FaPlus />
+              <span>Add Expense</span>
+            </Link>
+            <a href="/expenses/raw">
+              <FaDownload />
+              <span>Load Raw Data</span>
+            </a>
+          </section>
+          {hasExpenses && <ExpensesList expenses={expenses} />}
+          {!hasExpenses && <section id='no-expenses'>
+            <h1>No expenses found...</h1>
+            <p>Start <Link to="add">adding some</Link> today</p>
+          </section>}
         </main>
       </>
   );
@@ -30,6 +37,8 @@ export function links() {
   return [{rel: 'stylesheet', href: expensesStyles}]
 }
 
-export function loader() {
-  return getExpenses();
+export async function loader() {
+  const expenses = await getExpenses();
+
+  return expenses;
 }

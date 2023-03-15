11a485a79dd793bd844dd1bdf0007ce9d731b55f
Mykhailo Ostapysko <mykhailo.ostapysko@globant.com>
2023-03-15 10:36:26 +0200

    adds server-side validation

--- app/routes/_app.expenses.add.jsx
+++ app/routes/_app.expenses.add.jsx
@@ -2,7 +2,8 @@ import { redirect } from '@remix-run/node';
 import { useNavigate } from '@remix-run/react';
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
-import { addExpense } from '../data/expenses.server';
+import { addExpense } from '~/data/expenses.server';
+import { validateExpenseInput } from '~/data/validation.server';
 
 export default function AddExpensePage() {
   const navigate = useNavigate();
@@ -19,7 +20,12 @@ export default function AddExpensePage() {
 export async function action({ request }) {
   const formData = await request.formData();
   const expenseData = Object.fromEntries(formData);
-  console.log('expenseData', expenseData);
+  
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
 
   await addExpense(expenseData);
 

691795ef946abc8adc7a55dd243bf75d3910188e
Mykhailo Ostapysko <mykhailo.ostapysko@globant.com>
2023-03-16 09:13:12 +0200

    adds real data and error handling  for statistics page

--- app/routes/_app.expenses_.analysis.jsx
+++ app/routes/_app.expenses_.analysis.jsx
@@ -1,32 +1,52 @@
+import { Link, useLoaderData, useCatch } from "@remix-run/react";
+
 import ExpenseStatistics from '~/components/expenses/ExpenseStatistics';
 import Chart from '~/components/expenses/Chart';
+import Error from '~/components/util/Error';
 
 import analysisStyles from '~/styles/analysis.css';
-
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'first expense',
-    amount: 12.99,
-    date: new Date().toISOString(),
-  },
-  {
-    id: 'e2',
-    title: 'second expense',
-    amount: 4.99,
-    date: new Date().toISOString(),
-  },
-]
+import { getExpenses } from '../data/expenses.server';
+import { json } from "react-router";
 
 export default function ExpensesAnalysisPage() {
+  const expenses = useLoaderData();
+
   return (
       <main>
-        <Chart expenses={DUMMY_EXPENSES} />
-        <ExpenseStatistics expenses={DUMMY_EXPENSES} />
+          <Chart expenses={expenses} />
+          <ExpenseStatistics expenses={expenses} /> 
       </main>
   );
 }
 
 export function links() {
   return [{rel: 'stylesheet', href: analysisStyles}]
+}
+
+export async function loader() {
+  const expenses = await getExpenses();
+
+  const hasExpenses = expenses && expenses.length > 0;
+
+  if (!hasExpenses) {
+    throw json(
+      { message: 'No expenses so far...' },
+      {
+        status: 404,
+        statusText: 'Expenses not found',
+      }
+    )
+  }
+
+  return expenses;
+}
+
+export function CatchBoundary() {
+  const caughtResponse = useCatch();
+
+  return <main>
+    <Error title={caughtResponse.message}>
+      <p>{caughtResponse.data?.message || 'Something went wrong...'}</p>
+    </Error>
+  </main>
 }
